f08a5ddfe329ae758d7c5add61df1371698663e4
fallenarchon <joeamrhein@gmail.com>
2013-07-28 14:47:32 -0700

    UI tweaks for App Selection List

--- NewRelicAndroidPOC/src/main/java/com/arconus/newrelicpoc/fragments/ApplicationsSelectionListFragment.java
+++ NewRelicAndroidPOC/src/main/java/com/arconus/newrelicpoc/fragments/ApplicationsSelectionListFragment.java
@@ -28,29 +28,29 @@ public class ApplicationsSelectionListFragment extends Fragment {
     private BaseAdapter getAdapter() {
         List<ApplicationsListDataOrb> list = new ArrayList<ApplicationsListDataOrb>();
 
-        list.add(new ApplicationsListDataOrb("Aggregator", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Aggregator 1", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Aggregator 2", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("BE", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("BE Tracker Dashboard", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Beacon 1", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Beacon 2", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Beacon 3", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Collector Proxy", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Dev", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Dirac Staging", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Documentation", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Integration", "128ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Aggregator", "5.6ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Aggregator 1", "0.00ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Aggregator 2", "226ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("BE", "142ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("BE Tracker Dashboard", "9.1ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Beacon 1", "0.56ms", "0.00% errors", R.color.NavDrawerRed));
+        list.add(new ApplicationsListDataOrb("Beacon 2", "0.00ms", "0.00% errors", R.color.NavDrawerRed));
+        list.add(new ApplicationsListDataOrb("Beacon 3", "18.5ms", "0.00% errors", R.color.NavDrawerYellow));
+        list.add(new ApplicationsListDataOrb("Collector Proxy", "414ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Dev", "5.6ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Dirac Staging", "0.00ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Documentation", "312ms", "0.00% errors", R.color.NavDrawerYellow));
+        list.add(new ApplicationsListDataOrb("Integration", "6.5ms", "0.00% errors", R.color.NavDrawerGreen));
         list.add(new ApplicationsListDataOrb("Our People", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Pinger Production", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Pinger Server Production", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("RPM UI", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Staging", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Staging Aggregator", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Staging Beacon", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Staging Pinger - Server", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("Storefront", "128ms", "0.00% errors", R.color.NavDrawerGreen));
-        list.add(new ApplicationsListDataOrb("System Uptime Dashboard", "128ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Pinger Production", "5.6ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Pinger Server Production", "0.00", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("RPM UI", "226ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Staging", "142ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Staging Aggregator", "9.1ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Staging Beacon", "0.56ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Staging Pinger - Server", "0.00ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("Storefront", "18.6ms", "0.00% errors", R.color.NavDrawerGreen));
+        list.add(new ApplicationsListDataOrb("System Uptime Dashboard", "414ms", "0.00% errors", R.color.NavDrawerGreen));
 
         return new ApplicationsListAdapter(getActivity(), R.layout.app_selection_list_item, list);
     }
